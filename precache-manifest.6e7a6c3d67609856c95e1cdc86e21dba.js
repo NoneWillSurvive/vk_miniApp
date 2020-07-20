e3e50ac5a8f3bb417f72c8aff6198de70d7cf6e6
Kirill Bogdanov <kab2000@bk.ru>
2020-07-20 11:20:13 +0300

    Updates

---
+++ precache-manifest.6e7a6c3d67609856c95e1cdc86e21dba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35e35779859afa2e5f59b74024c81fcf",
+    "url": "/vk_miniApp/index.html"
+  },
+  {
+    "revision": "5c40c2ed58d7899bc8df",
+    "url": "/vk_miniApp/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "5c40c2ed58d7899bc8df",
+    "url": "/vk_miniApp/static/js/2.3bd57d83.chunk.js"
+  },
+  {
+    "revision": "1c9a5c14c094f70c35a5a57a5770d494",
+    "url": "/vk_miniApp/static/js/2.3bd57d83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c9662aefb5329ef286",
+    "url": "/vk_miniApp/static/js/main.ee9ce633.chunk.js"
+  },
+  {
+    "revision": "16f590b08635dc931d0c",
+    "url": "/vk_miniApp/static/js/runtime-main.f0bdae4b.js"
+  }
+]);
